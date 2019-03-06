264fd2315a506f111e91de9fd1f0bddffad32724
Karin Ohman <kaohma@gmail.com>
2019-03-06 16:14:04 -0700

    Add postgres database and setup server to user database

--- app.js
+++ app.js
@@ -3,65 +3,54 @@ const app = express();
 import cors from 'cors';
 app.use(cors());
 app.use(express.json());
-import shortid from 'shortid';
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
-// const users = client.db('bike-see').collection('users');
-// const favorites = client.db('bike-see').collection('favorites');
-
-app.locals.users = [
-  { id: '1', name: 'Karin', email: 'k@k', password: 'k' }
-];
-app.locals.favorites = [];
-
-app.get('/api/v1/users', (req, res) => {
-  res.status(200).json(app.locals.users);
+app.get('/api/v1/users', async (req, res) => {
+  const users = await database('users').select()
+  res.status(200).json(users);
 });
 
-app.post('/api/v1/users', (req, res) => {
+app.post('/api/v1/users', async (req, res) => {
   const { email, password } = req.body;
   if ( !email || !password ) return res.status(422).json('Please provide an email and password');
-  const user = app.locals.users.find(user => user.email === email);
+  const users = await database('users').select();
+  const user = users.find(user => user.email === email);
   if (!user) return res.status(404).json('User not found');
   if (password !== user.password) return res.status(422).json('Password is incorrect');
   return res.status(200).json(user);
 });
 
-app.post('/api/v1/users/new', (req, res) => {
+app.post('/api/v1/users/new', async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) return res.status(422).json('Please provide a name, email and password');
-  const existingUser = app.locals.users.find(user => user.email === email);
+  const users = await database('users').select();
+  const existingUser = users.find(user => user.email === email);
   if (existingUser) return res.status(422).json('User already exists');
-  const newUser = {
-    id: shortid.generate(),
-    name,
-    email,
-    password
-  };
-  app.locals.users.push(newUser);
-  return res.status(201).json(newUser);
+  const newUser = await database('users').insert({ name, email, password }, 'id')
+  return res.status(201).json({id: newUser[0]});
 });
 
-app.post('/api/v1/users/favorites/new', (req, res) => {
+app.post('/api/v1/users/favorites/new', async (req, res) => {
   const { user_id, station_id } = req.body;
   if (!user_id || !station_id) return res.status(422).json('Please provide a user id and station id');
-  const newFavorite = {
-    user_id,
-    station_id
-  };
-  app.locals.favorites.push(newFavorite);
-  return res.status(201).json(newFavorite);
+  const newFavorite = await database('favorites').insert({ user_id, station_id }, 'id');
+  return res.status(201).json({id: newFavorite[0]});
 });
 
-app.get('/api/v1/users/:id/favorites', (req, res) => {
-  const favorites = app.locals.favorites.filter(favorite => favorite.user_id == req.params.id);
-  return res.status(200).json(favorites);
+app.get('/api/v1/users/:id/favorites', async (req, res) => {
+  const favorites = await database('favorites').select();
+  const userFavorites = favorites.filter(favorite => favorite.user_id == req.params.id);
+  return res.status(200).json(userFavorites);
 });
 
-app.delete('/api/v1/users/:id/favorites/:station_id', (req, res) => {
+app.delete('/api/v1/users/:id/favorites/:station_id', async (req, res) => {
   const { id, station_id } = req.params;
-  const index = app.locals.favorites.findIndex(favorite => favorite.user_id === id && favorite.station_id === station_id);
-  if (index === -1) return res.status(404).json('Favorite not found')
-  app.locals.favorites.splice(index, 1)
+  const favorites = await database('favorites').select();
+  const favorite = favorites.find(favorite => favorite.user_id === id && favorite.station_id === station_id);
+  if (!favorite) return res.status(404).json('Favorite not found')
+  await database('favorites').where({ id: favorite.id }).del();
   return res.sendStatus(204);
 });
 
--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 import app from './app';
-// const MongoClient = require('mongodb').MongoClient;
-// import password from './db-key';
-
-// const uri = `mongodb+srv://kaohma:${password}@cluster0-drsae.mongodb.net/test?retryWrites=true`;
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-
 app.set('port', 3001);
 
-// client.connect(err => {
-//   if (err) return console.log(err);
-
-  app.listen(app.get('port'), () => {
-    console.log(`App is running on http://localhost:${app.get('port')}.`)
-  });
-
-//   client.close();
-// });
-
+app.listen(app.get('port'), () => {
+  console.log(`App is running on http://localhost:${app.get('port')}.`)
+});
