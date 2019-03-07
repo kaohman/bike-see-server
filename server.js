9355e603a369e77c99f5d11c4a9f975e580c3fda
Karin Ohman <kaohma@gmail.com>
2019-03-06 17:30:24 -0700

    Update port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from './app';
-app.set('port', 3001);
+app.set('port', process.env.PORT || 3001);
 
 app.listen(app.get('port'), () => {
   console.log(`App is running on http://localhost:${app.get('port')}.`)
