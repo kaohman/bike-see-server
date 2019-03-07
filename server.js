7199625bedf050b6298f94791ccb804bbe8a0f9a
Karin Ohman <kaohma@gmail.com>
2019-03-06 17:45:01 -0700

    Update server file

--- server.js
+++ server.js
@@ -4,3 +4,20 @@ app.set('port', process.env.PORT || 3001);
 app.listen(app.get('port'), () => {
   console.log(`App is running on http://localhost:${app.get('port')}.`)
 });
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
